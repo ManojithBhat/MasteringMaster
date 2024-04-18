dfa3e6c1b94c543afd4aa27f8e3d1142dab538cc
ManojithBhat <120463312+ManojithBhat@users.noreply.github.com>
2024-04-18 11:03:48 +0530

    Update anchor tag

--- src/resource/components/IndividualLink.jsx
+++ src/resource/components/IndividualLink.jsx
@@ -6,7 +6,7 @@
        <>
       <div className="bg-[#333645] rounded-lg p-4 flex justify-between align-middle">
         <div className="text-white font-medium">{name}</div>
-        <a href={link} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">
+        <a href={link} target="_blank" className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">
             Click here 
           </a>
       </div>
