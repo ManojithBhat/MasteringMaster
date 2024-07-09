7aca1c3a0d38a01b53f900ad9ece0e46dbf8d4a5
ManojithBhat <manojithbhat@gmail.com>
2024-07-09 17:17:00 +0530

    change heading in quiz

--- src/quiz/components/Header.jsx
+++ src/quiz/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function HeaderQuiz() {
     return (
       <header id="headerQuiz">
-        <h1>Did you become a master?</h1>
+        <h1>Did you become a master in this topic?</h1>
       </header>
     );
   }
